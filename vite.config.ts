33ca5bb0aa4ef2af57b0bc8e6fca33f515c64f90
amiraghelan <amirhaghelan@gmail.com>
2025-07-29 22:29:21 +0330

    add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+       alias: {
+         "@styles": path.resolve(__dirname, "src/styles"),
+         "@components": path.resolve(__dirname, "src/components"),
+         "@pages": path.resolve(__dirname, "src/pages")
+       },
+     },
+     css: {
+       preprocessorOptions: {
+         scss: {
+           additionalData: `@use "@styles/abstracts/variables" as vars;\n`,
+         },
+       },
+     },
 })
