98824524ca34f71c9f23c6a7b98f5211736ea9b7
amiraghelan <amirhaghelan@gmail.com>
2025-08-05 23:44:37 +0330

    fix admin panel issue

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -5,38 +5,49 @@ import AdminPanelProducts from "@components/AdminPanelProducts";
 import "@styles/components/AdminPanel.scss";
 
 const AdminPanel = () => {
-  const authContext = useContext(AuthContext);
+   const authContext = useContext(AuthContext);
 
-  if (!authContext) throw new Error("AuthContext must be used within AuthProvider");
+   if (!authContext)
+      throw new Error("AuthContext must be used within AuthProvider");
 
-  const { user } = authContext;
+   const { user } = authContext;
 
-  if (user.role !== "admin") {
-    return <div className="admin-panel">Access denied. Admins only.</div>;
-  }
+   if (user.role !== "admin") {
+      return <div className="admin-panel">Access denied. Admins only.</div>;
+   }
 
-  return (
-    <div className="admin-panel">
-      <div className="admin-container">
-        <div className="admin-header">
-          <h1 className="admin-title">Admin Panel</h1>
-        </div>
-        <div className="tabs">
-          <NavLink
-            to="/admin/products"
-            className={({ isActive }) => `tab ${isActive ? "active" : ""}`}
-          >
-            Products
-          </NavLink>
-        </div>
-        <div className="tab-content">
-          <Routes>
-            <Route path="products" element={<AdminPanelProducts />} />
-          </Routes>
-        </div>
+   return (
+      <div className="admin-panel">
+         <div className="admin-container">
+            <div className="admin-header">
+               <h1 className="admin-title">Admin Panel</h1>
+            </div>
+            <div className="tabs">
+               <NavLink
+                  to="/admin/products"
+                  className={({ isActive }) =>
+                     `tab ${isActive ? "active" : ""}`
+                  }>
+                  Products
+               </NavLink>
+            </div>
+            <Routes>
+               <Route
+                  path=""
+                  element={<></>}
+               />
+               <Route
+                  path="products"
+                  element={
+                     <div className="tab-content">
+                        <AdminPanelProducts />
+                     </div>
+                  }
+               />
+            </Routes>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default AdminPanel;
